e950e0f1ab5fead60eb3d06669cf45a8d71f352f
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-17 01:18:25 +0600

    filter-action-types-and-action-problem-solved

--- src/Redux/Filter/Reducers/FilterReducers.js
+++ src/Redux/Filter/Reducers/FilterReducers.js
@@ -9,15 +9,17 @@ const FilterReducers = (state = initialFilterState, action) => {
             return { ...state, status: action.payload }
         }
         case COLORSHOW: {
-            const matchedColor = state.colors?.map(data => {
-                if (data === action.payload) {
-                    return data;
-                } else {
-                    return { ...data, colors: action.payload }
-                }
-            })
-            return { ...state, colors: [...state.colors, action.payload] };
-        }
+            const colorIndex = state.colors.indexOf(action.payload);
+        
+            if (colorIndex === -1) {
+                // If color is not in the array, add it
+                return { ...state, colors: [...state.colors, action.payload] };
+            } else {
+                // If color is already in the array, remove it
+                const updatedColors = state.colors.filter(color => color !== action.payload);
+                return { ...state, colors: updatedColors };
+            }
+        }        
         default: {
             return state;
         }
