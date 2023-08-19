d7dc8719c3c7a0ebb40c37c9f144bfdd9049cedf
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-20 05:49:15 +0600

    project-build-push

--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -1,9 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
-import {composeWithDevTools} from "redux-devtools-extension"
-import  rootReducer  from "../Todo/Reducers/rootReducer";
-import ToDoReducer from "../Todo/Reducers/ToDoReducer";
+import { composeWithDevTools } from "redux-devtools-extension"
+import rootReducer from "../Todo/Reducers/RootReducer";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+const Store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
-export default store;
+export default Store;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './Redux/Store/Store.js'
+import Store from './Redux/Store/Store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={Store}>
       <App />
     </Provider>
   </React.StrictMode>,
